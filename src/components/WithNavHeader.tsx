050ed9ac5f34e4087ac0ce43ea3b964f5f1e5d3e
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-07-23 13:28:32 -0500

    Create WithNavHeader.tsx

---
+++ src/components/WithNavHeader.tsx
@@ -0,0 +1,25 @@
+import {Box, Button, IconButton, Typography} from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export default function (){
+
+    return(
+        <Box
+            component={'header'}
+            sx={{display:"flex", flexDirection:"row", mb:"auto", pl:8, pr:4, py:2, backgroundColor:"#000000"}}
+        >
+            <Box
+                sx={{display:"flex", flexDirection:"row"}}
+                mr={"auto"}
+            >
+                <Typography variant={"h5"} display="inline" color={"#ffffff"} >Tongue </Typography>
+                <Typography variant={"h5"} color={"#6ac53c"} display="inline" >&nbsp;for Merchants</Typography>
+            </Box>
+            <Button sx={{justifyContent:"flex-end", color:"#ffffff", textTransform:"capitalize" }} >Iniciar Sesión</Button>
+            <IconButton sx={{justifyContent:"flex-end", color:"#ffffff" }} >
+                <ArrowBackIcon/>
+            </IconButton>
+        </Box>
+    )
+
+}
