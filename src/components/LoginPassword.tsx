a83e8af76f66c8abc19a852ec96b15d3281eef6e
Rommel <rommel.valdiviezo@epn.edu.ec>
2022-08-14 22:32:42 -0500

    Login

--- src/components/LoginPassword.tsx
+++ src/components/LoginPassword.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Container, TextField, Typography} from "@mui/material";
+import {Button, Container, TextField, Typography} from "@mui/material";
 
 export default function (){
 
@@ -17,12 +17,11 @@ export default function (){
             </Typography>
             <Typography variant={"h5"} mb={1} >Inicio de Sesión</Typography>
             <Typography variant={"body2"} mb={4} >Usa tu dirección de correo electrónico y tu contraseña para iniciar sesión en el Administrador de Uber Eats</Typography>
-            <Typography variant={"subtitle1"} mb={1} color={"#707070"} >Correo electrónico</Typography>
-            <TextField variant={"outlined"} fullWidth={true} placeholder={"Ingrese su email"}
+            <Typography variant={"subtitle1"} mb={1} color={"#707070"} >Contraseña</Typography>
+            <TextField variant={"outlined"} fullWidth={true} placeholder={"Ingrese su contraseña"}
                        sx={{mb:2}}
             ></TextField>
             <Button variant={"contained"} fullWidth={true} sx={{py:1.5,backgroundColor:"#529930", mb:1}} >Siguiente</Button>
-            <Button variant={"contained"} fullWidth={true} sx={{py:1.5}} >Continuar con Google</Button>
         </Container>
     )
 
